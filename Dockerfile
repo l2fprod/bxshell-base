a7f3c80bde7773e7811efa2bcb1b38ee6779bb6b
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-02-06 16:47:03 +0100

    added latest terraform

--- Dockerfile
+++ Dockerfile
@@ -1,44 +1,4 @@
 FROM ubuntu:16.04
-RUN apt-get -qq update && apt-get -qq install -y \
-  apt-transport-https \
-  bash-completion \
-  ca-certificates \
-  curl \
-  figlet \
-  inetutils-ping \
-  jq \
-  libasound2 \
-  libgconf2-dev \
-  libgtkextra-dev \
-  libnss3 \
-  libx11-xcb1 \
-  libxss1 \
-  libxtst-dev \
-  nano \
-  python \
-  python-virtualenv \
-  python-setuptools \
-  python-pip \
-  software-properties-common \
-  sudo \
-  tmux \
-  unzip \
-  wget \
-  zip
 
-# Docker in Docker
-RUN apt-get -qq remove docker docker-engine docker.io
-RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-RUN add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-RUN apt-get -qq update && apt-get -qq -y install docker-ce
-
-# Yarn
-RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
-  apt-get -qq update && apt-get install -qq -y yarn
-
-# Cleanup
-RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+COPY install.sh install.sh
+RUN ./install.sh && rm install.sh
---
+++ install.sh
@@ -0,0 +1,55 @@
+echo "Installing dependencies..."
+apt-get -qq update && apt-get -qq install -y \
+  apt-transport-https \
+  bash-completion \
+  ca-certificates \
+  curl \
+  figlet \
+  inetutils-ping \
+  jq \
+  libasound2 \
+  libgconf2-dev \
+  libgtkextra-dev \
+  libnss3 \
+  libx11-xcb1 \
+  libxss1 \
+  libxtst-dev \
+  nano \
+  python \
+  python-virtualenv \
+  python-setuptools \
+  python-pip \
+  software-properties-common \
+  sudo \
+  tmux \
+  unzip \
+  wget \
+  zip
+
+# Docker in Docker
+echo "Docker in Docker"
+apt-get -qq remove docker docker-engine docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get -qq update && apt-get -qq -y install docker-ce
+
+# Terraform
+echo "Terraform"
+curl -LO "https://releases.hashicorp.com/terraform/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')_linux_amd64.zip" && \
+  unzip terraform*.zip && \
+  rm -f terraform*.zip && \
+  chmod 755 terraform && \
+  mv terraform /usr/local/bin
+
+# Yarn
+echo "Yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+  apt-get -qq update && apt-get install -qq -y yarn
+
+# Cleanup
+echo "Cleanup"
+apt-get clean && rm -rf /var/lib/apt/lists/*
