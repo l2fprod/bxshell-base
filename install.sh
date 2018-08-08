79de6441dbf1b15edfe5ddf6196398654573b021
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-08-08 17:02:36 +0200

    colors for nano

--- install.sh
+++ install.sh
@@ -56,6 +56,10 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
   apt-get -qq update && apt-get install -qq -y yarn
 
+# Colors for nano
+echo "Colors for nano"
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
 # Cleanup
 echo "Cleanup"
 apt-get clean && rm -rf /var/lib/apt/lists/*
